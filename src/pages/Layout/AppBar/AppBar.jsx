8911185424e5f42d48e10f186b1faf90b430046c
Malakin1116 <130695935+Malakin1116@users.noreply.github.com>
2024-11-16 13:02:31 +0200

    ?

--- src/pages/Layout/AppBar/AppBar.jsx
+++ src/pages/Layout/AppBar/AppBar.jsx
@@ -10,7 +10,7 @@ export default function AppBar() {
   return (
     <header>
       <Navigation />
-      {!isLoggedIn ? <UserMenu /> : <AuthNav />}
+      {isLoggedIn ? <UserMenu /> : <AuthNav />}
     </header>
   );
 }
